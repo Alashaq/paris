8aba7d48187cc2ce17d1cc7930a0de23f16c9514
AlashaqSultanFuckAll <40842773+AlashaqSultanFuckAll@users.noreply.github.com>
2020-02-07 17:33:31 -1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ const gif = require("gif-search");
 const client = new Discord.Client({disableEveryone: true});
 
 
-const prefix = "-";
+const prefix = "*";
 /////////////////////////
 ////////////////////////
 
